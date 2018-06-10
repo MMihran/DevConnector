3c2772961fa3c4894d6fa8ca99b040ac50df1a95
MMihran <margaryan_mihran@mail.ru>
2018-06-10 13:21:06 +0400

    authorization with jwt token

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://uzuz:smbo0396@ds253960.mlab.com:53960/devconnectordb"
+  mongoURI: "mongodb://uzuz:smbo0396@ds253960.mlab.com:53960/devconnectordb",
+  secretOrKey: "secret"
 };
